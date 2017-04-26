31fcee1bcb3151ac94a0674b50c99f65cc897c2b
Alex Cannon <alexcannon2@live.com>
2017-04-26 17:53:17 -0400

    navigation changes along

--- app/src/main/java/edu/iupui/jamcanno/listenup/SingleFragmentActivity.java
+++ app/src/main/java/edu/iupui/jamcanno/listenup/SingleFragmentActivity.java
@@ -93,13 +93,9 @@ public abstract class SingleFragmentActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
         }
+
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
